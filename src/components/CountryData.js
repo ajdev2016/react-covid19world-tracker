4938f787393a65faddd4832c14e748a08aa5f3d5
Aj Nocolai Garrigues <ajgarrigues@rocketmail.com>
2020-04-18 22:43:31 +0800

    data and ui completion

---
+++ src/components/CountryData.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import {Col} from 'reactstrap'
+
+export class CountryData extends Component {
+
+    getFlag = str => {
+        str = str.replace(/^\s+|\s+$/g, ''); // trim
+        str = str.toLowerCase();
+    
+        // remove accents, swap ñ for n, etc
+        var from = "àáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+        var to   = "aaaaaeeeeiiiioooouuuunc------";
+
+        for (var i=0, l=from.length ; i<l ; i++) {
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+        }
+
+        str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+            .replace(/\s+/g, '-') // collapse whitespace and replace by -
+            .replace(/-+/g, '-'); // collapse dashes
+        return `https://assets.thebasetrip.com/api/v2/countries/flags/${str}.png`
+    }
+
+    render() {
+        const {country, confirmed, recovered, deaths, color} = this.props
+        // console.log(color)
+
+        const card  = {
+            backgroundColor: color
+        }
+
+        const info = {
+            marginBottom: '3px'
+        }
+
+        return (
+            <Col xs="6" sm="6" md="4" lg="3" className="text-center p-3" style={card}>
+                <img className="mb-2" src={this.getFlag(country)} width="50" />
+                <h4>{country}</h4> 
+                <p style={info}>Confirmed: {confirmed}</p> 
+                <p style={info}>Recovered: {recovered}</p>  
+                <p style={info}>Deaths: {deaths}</p>    
+            </Col>
+        )
+    }
+}
+
+export default CountryData
---
+++ src/components/Home.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import {Col, Row} from 'reactstrap'
+import axios from 'axios'
+import moment from 'moment'
+import CountryData from './CountryData'
+
+export class Home extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            covid_data: null,
+            startDate: null
+        }
+
+        this.fetchData()
+    }
+
+    fetchData = async () =>  {
+
+        try {
+            const covid_data = await axios.get('https://pomber.github.io/covid19/timeseries.json')
+            const {data} = covid_data
+            const {startDate} = this.state
+
+            const start_date = startDate ? startDate : moment().subtract(1, 'day').format('YYYY-M-D')
+
+            let result  = []
+            Object.keys(data).map(key => {
+                data[key].map(item2 => {
+                    if (item2.date === start_date ) {
+                        item2.country = key
+                        result.push(item2)
+                    }
+                })
+            })
+            // console.log(result)
+            
+            this.setState({ 
+                covid_data: result,
+                startDate:start_date 
+            })
+
+        } catch (error) {   
+            console.log(error)
+        }
+    }
+
+    generetaTotalCases = () => {
+        const { covid_data } = this.state
+        return covid_data.map(data => <CountryData {...data} color={this.generateRandomColor(data.country)} />)
+    }
+
+    generateRandomColor = name => {
+        let letters = 'BCDEF'.split('');
+        let color = '#';
+        for (let i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * letters.length)];
+        }
+        return color;
+    }
+    
+    render() {
+        const { covid_data, startDate } = this.state
+        return (
+            <>
+                <Row className="no-gutters">
+                    <Col xs="12" className="text-center" style={{backgroundColor: 'rgb(222, 236, 253)'}}><h2 className="p-3">Covid-19 World Tracker</h2></Col>
+                    {
+                        covid_data  && this.generetaTotalCases()
+                    }
+                </Row>    
+            </>
+        )
+    }
+
+    
+}
+
+export default Home
